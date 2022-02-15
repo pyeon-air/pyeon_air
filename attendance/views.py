608bda85646970aa1626a1d6ce06abd57bd1efa8
codongmin <codongmin@gmail.com>
2022-02-15 16:35:58 +0900

    PA-82 QR view 초안작성

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import InOutLogSerializer, QrInfoSerializer
+from .models import InOutLog, QrInfo
+
+
+# QR
+class QrApi(APIView):
+    def get(self, request):
+        queryset = QrInfo.objects.all()
+        serializer = QrInfoSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        return 
+
+
+# 입퇴실 인원 현황
+class API(APIView):
+    def get(self, request):
+        return
+
+    def post(self, request):
+        return 
+
+
+# 출석확인
+class API(APIView):
+    def get(self, request):
+        return
+
+    def post(self, request):
+        return 
