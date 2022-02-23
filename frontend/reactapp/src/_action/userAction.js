75f10b21c83860d5a589f78b25ce94d3593fc979
Donghun Ham <donghunham@Donghunui-MacBookPro.local>
2022-02-23 20:14:03 +0900

    update

--- frontend/reactapp/src/_action/userAction.js
+++ frontend/reactapp/src/_action/userAction.js
@@ -1,7 +1,7 @@
 import { LOGIN_USER } from "./type";
 import { request } from "../axios/axios";
 
-const USER_URL = "/management/member_api";
+const USER_URL = "/member/post_login";
 
 export function loginUser(dataToSubmit) {
     const data = request("get", USER_URL , dataToSubmit);
