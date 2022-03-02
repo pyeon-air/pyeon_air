e24242ac1412a569d50736396a48f2e54bb11876
jeihomme <jeihomme@nate.com>
2022-03-02 22:18:58 +0900

    로그인되도록 수정

--- frontend/reactapp/src/index.js
+++ frontend/reactapp/src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import promiseMiddlerware from "redux-promise";
-import reduxThunk from "redux-thunk";
+// import { Provider } from "react-redux";
+// import { applyMiddleware, createStore } from "redux";
+// import promiseMiddlerware from "redux-promise";
+// import reduxThunk from "redux-thunk";
 import App from './App';
 
 
 
 
 ReactDOM.render(
-  // Redux 할때 필요 
- 
   <BrowserRouter>
   <App />
-</BrowserRouter>
-,
+</BrowserRouter>,
   document.getElementById('root')
 );
 
