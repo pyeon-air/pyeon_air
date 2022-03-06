642684d1df0bf43989c3b0380907bc24c9f00979
highway92 <highway92@kakao.com>
2022-03-06 15:56:48 +0900

    feat : jwt token verify / refresh 기능 추가
    - 토큰 refresh 기능이 추가 되었습니다.
    - 토큰 verify 기능이 추가 되었습니다.
    - login관련 api명이 변경되었습니다.

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 from member import views
+from rest_framework_jwt.views import verify_jwt_token,refresh_jwt_token
 
 urlpatterns = [
-    path('post_login', views.UserLogInAPI.as_view()),
+    path('api/token', views.UserLogInAPI.as_view()),
+    path('api/token/verify', verify_jwt_token),
+    path('api/token/refresh', refresh_jwt_token)
+
 ]
